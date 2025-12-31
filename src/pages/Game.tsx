9827553564b59d33eb671ae6472cdde0b78e47aa
Vladislav Karachun <v.karachun@scalablesolutions.io>
2025-12-31 21:19:07 +0700

    card design

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -7,6 +7,7 @@ interface GameProps {
   onLeave: () => void;
 }
 
+
 export function Game({ players, currentPlayerId, onLeave: _onLeave }: GameProps) {
   const visiblePlayers = players.filter((player) => player.id !== currentPlayerId);
 
@@ -14,9 +15,9 @@ export function Game({ players, currentPlayerId, onLeave: _onLeave }: GameProps)
      <GridList
       items={visiblePlayers}
       renderItem={(p) => (
-        <div className="h-full w-full flex items-center justify-center gap-4 flex-col">
-          <div className="text-4xl text-gray-500">{p.name}</div>
-          <div className="text-4xl font-semibold">{p.word}</div>
+        <div className="relative h-full w-full flex items-center justify-center flex-col px-4">
+          <div className="absolute top-4 text-xl text-gray-500">{p.name}</div>
+          <div className="text-4xl text-center break-words whitespace-normal">{p.word}</div>
         </div>
       )}
     />
